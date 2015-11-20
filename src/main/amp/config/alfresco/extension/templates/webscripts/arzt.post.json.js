dc92e7c16caf3365b09685e84ba1b9df01f134f8
Anuar Suarez <anuar.suarez@hotmail.com>
2015-11-20 21:01:49 +0100

    pull arzt from node

---
+++ src/main/amp/config/alfresco/extension/templates/webscripts/arzt.post.json.js
@@ -0,0 +1,26 @@
+var aerzte = search.luceneSearch("TYPE:\"mu:aerzteregisterkarte\"");
+
+var contentType = "mu:aerzteregisterkarte";
+
+var properties = [];
+
+var status = {
+    "status":"error"
+};
+
+
+if (json.has("arztVorname") && json.has("arztNachname")) {
+
+    properties['mu:arztVorname'] = json.get("arztVorname");
+    properties['mu:arztNachname'] = json.get("arztNachname");
+
+    if (json.has("arztFachbereich"))
+        properties['mu:arztFachbereich'] =  json.get("arztFachbereich");
+
+    var arzt = companyhome.createNode(aerzte.length, contentType, properties);
+
+    if(arzt)
+        status.status = "ok";
+}
+
+model.status = jsonUtils.toJSONString(status);
