9182561ca624b5f25f5ecbdca127f0ad827f4d5e
Anuar Suarez <anuar.suarez@hotmail.com>
2015-11-20 22:23:54 +0100

    added id

--- src/main/amp/config/alfresco/extension/templates/webscripts/arzt.get.js
+++ src/main/amp/config/alfresco/extension/templates/webscripts/arzt.get.js
@@ -5,6 +5,7 @@ if (aerzte)
     for (var i = 0; i < aerzte.length; i++) {
         var arzt = {};
 
+        arzt.id = aerzte[i].name;
         arzt.arztVorname = aerzte[i].properties['mu:arztVorname'];
         arzt.arztNachname = aerzte[i].properties['mu:arztNachname'];
         arzt.arztFachbereich = aerzte[i].properties['mu:arztFachbereich'];
